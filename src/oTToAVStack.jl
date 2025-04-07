7ded8dd7b0be3b7441fe589a00bd5e0b933a880a
YJHJACK <143137350+YJHJACK@users.noreply.github.com>
2025-04-07 17:15:41 -0500

    Fix the Typo of oTToAVStack.jl

--- src/oTToAVStack.jl
+++ src/oTToAVStack.jl
@@ -1,4 +1,4 @@
-module ottoAVStack
+module oTToAVStack
 
 using VehicleSim
 using Sockets
@@ -7,4 +7,4 @@ using Serialization
 include("client.jl")
 include("example_project.jl")
 
-end # module ottoAVStack
+end # module oTToAVStack
