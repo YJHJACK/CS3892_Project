21e876ede8d8c1b1275746c76bc190e1d708d67c
YJHJACK <143137350+YJHJACK@users.noreply.github.com>
2025-04-22 19:21:27 -0500

    Update oTToAVStack.jl

--- src/oTToAVStack.jl
+++ src/oTToAVStack.jl
@@ -3,6 +3,9 @@ module oTToAVStack
 using VehicleSim
 using Sockets
 using Serialization
+using LinearAlgebra
+using StaticArrays
+using Base.Threads
 
 include("client.jl")
 include("example_project.jl")
